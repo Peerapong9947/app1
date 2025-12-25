849682bcd62c28f8aa04f6bbd770f169f77314b8
Peerapong9947 <s6703052411414@email.kmutnb.ac.th>
2025-12-25 11:40:14 +0700

    classcomponents

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import react from 'react';
 import './style.css'
 import { Content, Footer, Header } from './func-components';
+import Calendar from './class-components';
 
 
 function App() {
   return (
     <>
       <Header/>
+      <p><center><Calendar/></center></p>
       <Content/>
       <Footer/>
     </>
