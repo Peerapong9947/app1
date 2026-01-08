d17619c347222d26b84e564424deb56d677245a3
Peerapong9947 <s6703052411414@email.kmutnb.ac.th>
2026-01-08 11:37:41 +0700

    event object

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import { Content, Footer, Header } from './func-components';
 import Calendar from './class-components';
 import Button from './class-components';
 import { Calculator2 } from './calculator';
+import { EventData2 } from './event-data';
 
 
 function App() {
-  return <Calculator2/>
+  return <EventData2/>
 }
 export default App;
