d78dccfc428520d54e73e0ba4956347117bbb073
Peerapong9947 <s6703052411414@email.kmutnb.ac.th>
2026-01-08 11:15:26 +0700

    parameters

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import './style.css'
 import { Content, Footer, Header } from './func-components';
 import Calendar from './class-components';
 import Button from './class-components';
+import { Calculator2 } from './calculator';
 
 
 function App() {
-  return (
-    <Button/>
-  )
+  return <Calculator2/>
 }
 export default App;
