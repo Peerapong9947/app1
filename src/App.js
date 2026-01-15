790cc3e25b7b4c44c8bdacd1fee4644a39efbd67
Peerapong9947 <s6703052411414@email.kmutnb.ac.th>
2026-01-15 10:35:56 +0700

    References

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import react from 'react';
 import './style.css'
-import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
-import Button from './class-components';
-import { Calculator2 } from './calculator';
 import { EventData2 } from './event-data';
+import RefsArray from './refs-array';
 
 
 function App() {
-  return <EventData2/>
+  return <RefsArray/>
 }
 export default App;
