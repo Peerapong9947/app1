2720cc3ed5cbe221f5ca58058f0bb100d12f3634
Peerapong9947 <s6703052411414@email.kmutnb.ac.th>
2026-01-15 11:19:19 +0700

    statefunction

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import react from 'react';
 import './style.css'
 import { EventData2 } from './event-data';
 import RefsArray from './refs-array';
+import StateFunc from './state-func';
 
 
 function App() {
-  return <RefsArray/>
+  return <StateFunc/>
 }
 export default App;
