5d969512fee78e9e6ceabe8f1708557017726300
Peerapong9947 <s6703052411414@email.kmutnb.ac.th>
2026-02-12 10:05:00 +0700

    Navlink

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import react from 'react';
-import './style.css'
-import { EventData2 } from './event-data';
-import RefsArray from './refs-array';
-import StateFunc from './state-func';
-import React from 'react';
-import { userContext } from './context';
-import Content2 from './context-content2';
-import Header2 from './context-header2';
+import React from 'react'
+import Router2 from "./router2"
 
-
-
-function App() {
-  let [user, setUser] = React.useState('')
-  return(
-    <userContext.Provider value={[user, setUser]}>
-      <Header2/>
-      <Content2/>
-    </userContext.Provider>
-  )
+export default function App() {
+  return <Router2/>
 }
-export default App;
